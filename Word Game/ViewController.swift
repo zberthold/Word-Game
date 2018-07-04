62f971c3cedf7c6e97d34b4d87845af36a1100cb
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-04 13:19:10 -0500

    Initial Commit

---
+++ Word Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Game
+//
+//  Created by Zoe Berthold on 7/4/18.
+//  Copyright © 2018 Zoe Berthold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
